b46d06adcfe0ade3f4d891a38250405e59714b6a
liiatana <31036379+liiatana@users.noreply.github.com>
2019-12-08 22:39:09 +0300

    Update usr.ts

--- test/usr.ts
+++ test/usr.ts
@@ -1,9 +1,60 @@
 import { Users } from "./../classes/users";
-//import { Users } from "./../config/users.json";
+import {User} from "./../classes/user"
+import axios from 'axios';
+import { SSL_OP_EPHEMERAL_RSA } from "constants";
+import { version } from "punycode";
+import request from 'sync-request' ;
+import { setupMaster } from "cluster";
 
-let users=new Users();
-users.loadFromJsonFile("./config/users.json");
 
-users.getAny();
+let user=new User('', '');
 
-console.log(users);
+
+
+
+
+
+
+
+function setupUser() {
+    axios.get(`http://dsud-fk-msk/LanDocs.WebApi.NetCore/api/v1/admin/apiversion`)
+        .then(response => {
+            user.version = String(response.data)
+            console.log('User!!!! ' + user.version)                            
+        })
+        .catch(error => console.log('Error', error))           
+
+}
+
+
+
+
+
+
+
+axios.get(`http://dsud-fk-msk/LanDocs.WebApi.NetCore/api/v1/admin/apiversion`)
+        .then(response => makeTestAsync(response.data))
+        .catch(error => console.log('Error', error))       
+        
+
+function makeTestAsync(version: String) {
+    console.log("The version getted with Async request is " + version);    
+}
+
+
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+let req = new XMLHttpRequest();
+req.open('GET', `http://dsud-fk-msk/LanDocs.WebApi.NetCore/api/v1/admin/apiversion`, false);  // `false` makes the request synchronous
+req.send(null);
+
+var myVersion = "unknown"
+if (req.status == 200) {
+    myVersion = req.responseText  
+}
+
+console.log("The version getted with XMLHttpRequest is " + myVersion);
+
+var res = request('GET', 'http://dsud-fk-msk/LanDocs.WebApi.NetCore/api/v1/admin/apiversion');
+myVersion = String(res.getBody());
+
+console.log("The version getted with syn-request is " + myVersion);
